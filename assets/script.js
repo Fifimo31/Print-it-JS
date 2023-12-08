a3dcb1677de5c0132e77b8aae53b7081794f891e
Hawa Sall <sallhawa30@yahoo.fr>
2023-12-08 16:53:37 +0100

    debut exercice

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,50 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+/*const emplacementFléche = () => {
+	const bannerFléche = document.getElementById("banner")
+	const flécheGauche = document.createElement(".arrow_left")
+	const flécheDroite = document.createElement(".arrow_right")
+	flécheGauche.setAttribute = ("src","images/arrow_left.png")
+	bannerFléche.appendChild(flécheGauche)
+}
+emplacementFléche()*/
+const emplacementFleche = () => {
+    const bannerFleche = document.getElementById("banner");
+    
+    // Créer l'élément image pour la flèche gauche
+    const flecheGauche = document.createElement("img");
+    flecheGauche.src = "assets/images/arrow_left.png";
+    flecheGauche.alt = "Flèche gauche";
+    flecheGauche.classList.add("arrow_left");
+
+    // Créer l'élément image pour la flèche droite
+    const flecheDroite = document.createElement("img");
+    flecheDroite.src = "assets/images/arrow_right.png";
+    flecheDroite.alt = "Flèche droite";
+    flecheDroite.classList.add("arrow_right");
+
+    // Ajouter les flèches au document
+    bannerFleche.appendChild(flecheGauche);
+    bannerFleche.appendChild(flecheDroite);
+};
+
+// Appeler la fonction
+
+
+const défilementImage = (slides) => {
+	emplacementFleche();
+	slides[0]
+	let flecheDroite = document.querySelector("arrow_right")
+	
+	flecheDroite.addEventListener("click",(e) => {
+		for(let i = 0; i< slides.length; i++){
+			console.log(slides.length)
+		
+		}
+	})
+	
+	
+}
+défilementImage(slides)
